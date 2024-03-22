0f8192c0633c783e47f9b29bf78d50c9d8c779a8
jyuan20 <124369673+jyuan20@users.noreply.github.com>
2024-03-22 07:57:39 +0000

    Delete sc22jyt/my_project directory

--- sc22jyt/my_project/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from . import views
-
-urlpatterns = [
-    path('api/login/', views.api_login, name='api_login'),
-    path('api/logout/', views.api_logout, name='api_logout'),
-    path('api/stories/', views.api_stories, name='api_stories'),
-    path('api/stories/<int:key>/', views.api_delete_story, name='api_delete_story')
-]
